cc3dabd3cab8e138b0bbe295020b6a4d044e9c5a
Pouic <loic.rodier@gmail.com>
2013-12-09 19:55:33 +0100

    distribManag info

---
+++ nexus-test-api/src/main/java/com/github/pouicr/HelloBean.java
@@ -0,0 +1,11 @@
+package com.github.pouicr;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pouic
+ * Date: 09/12/13
+ * Time: 19:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloBean {
+}
---
+++ nexus-test-infra/src/main/java/com/github/pouicr/InfraBean.java
@@ -0,0 +1,11 @@
+package com.github.pouicr;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pouic
+ * Date: 09/12/13
+ * Time: 19:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class InfraBean {
+}
